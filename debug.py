7d2d567d0898be1372a4f200abf75d6ee9d07f63
Cut0 <zeron9421@gmail.com>
2021-12-21 18:40:43 +0900

    fix: drawを追加した

--- debug.py
+++ debug.py
@@ -8,5 +8,7 @@ df = pd.read_csv(file_name, header=None)
 df = df[df[3] != 'unsettled']
 win_count = len(df[df[3] == 'win'])
 lose_count = len(df[df[3] == 'bust']) + len(df[df[3] == 'lose'])
+draw_count = len(df[df[3] == 'draw'])
 
-print('勝率:', win_count/(win_count+lose_count))
+print('勝率:', win_count/(win_count+lose_count+draw_count))
+print('敗率:', lose_count/(win_count+lose_count+draw_count))
