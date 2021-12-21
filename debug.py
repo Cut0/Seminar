ad7513c7b2b629a0d158cbd4a3441734f4120216
Cut0 <zeron9421@gmail.com>
2021-12-21 18:33:58 +0900

    update: 勝率を割り出すコード追加

---
+++ debug.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sys
+
+file_name = 'log/ai_player_log.csv' if len(
+    sys.argv) < 2 else 'log/ai_model_player_log.csv'
+
+df = pd.read_csv(file_name, header=None)
+df = df[df[3] != 'unsettled']
+win_count = len(df[df[3] == 'win'])
+lose_count = len(df[df[3] == 'bust']) + len(df[df[3] == 'lose'])
+
+print('勝率:', win_count/(win_count+lose_count))
